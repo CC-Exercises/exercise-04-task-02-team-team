36b076c6085821b8eccdc24a57fa6dcc9da5ff73
Steven Grossmann <grossmsn@live.de>
2017-05-12 00:04:23 +0200

    SQS QueueTextProcessor implementation start

--- src/main/java/de/ustutt/iaas/cc/core/QueueTextProcessor.java
+++ src/main/java/de/ustutt/iaas/cc/core/QueueTextProcessor.java
@@ -1,8 +1,10 @@
 package de.ustutt.iaas.cc.core;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
@@ -24,93 +26,116 @@ import de.ustutt.iaas.cc.TextProcessorConfiguration;
 
 public class QueueTextProcessor implements ITextProcessor {
 
-    private QueueSession session;
-    private QueueSender sender;
-    private QueueReceiver receiver;
+	private QueueSession session;
+	private QueueSender sender;
+	private QueueReceiver receiver;
 
-    public QueueTextProcessor(TextProcessorConfiguration conf) {
-	super();
-	try {
-	    Context jndi = null;
-	    QueueConnectionFactory conFactory = null;
-	    switch (conf.mom) {
-	    case ActiveMQ:
-		// initialize JNDI context
-		Hashtable<String, String> env = new Hashtable<String, String>();
-		env.put("java.naming.factory.initial", "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-		env.put("java.naming.security.principal", "system");
-		env.put("java.naming.security.credentials", "manager");
-		if (conf.activeMQurl == null) {
-		    env.put("java.naming.provider.url", "tcp://localhost:61616");
-		} else {
-		    env.put("java.naming.provider.url", conf.activeMQurl);
+	public QueueTextProcessor(TextProcessorConfiguration conf) {
+		super();
+		try {
+			Context jndi = null;
+			QueueConnectionFactory conFactory = null;
+			switch (conf.mom) {
+			case ActiveMQ:
+				// initialize JNDI context
+				Hashtable<String, String> env = new Hashtable<String, String>();
+				env.put("java.naming.factory.initial", "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+				env.put("java.naming.security.principal", "system");
+				env.put("java.naming.security.credentials", "manager");
+				if (conf.activeMQurl == null) {
+					env.put("java.naming.provider.url", "tcp://localhost:61616");
+				} else {
+					env.put("java.naming.provider.url", conf.activeMQurl);
+				}
+				env.put("connectionFactoryNames", "ConnectionFactory");
+				env.put("queue." + conf.requestQueueName, conf.requestQueueName);
+				env.put("queue." + conf.responseQueueName, conf.responseQueueName);
+				// create JNDI context (will also read jndi.properties file)
+				jndi = new InitialContext(env);
+				// connect to messaging system
+				conFactory = (QueueConnectionFactory) jndi.lookup("ConnectionFactory");
+				break;
+			case SQS:
+			default:
+				// Create the connection factory using the properties file
+				// credential provider.
+				conFactory = SQSConnectionFactory.builder().withRegion(Region.getRegion(Regions.US_WEST_2))
+						.withAWSCredentialsProvider(new PropertiesFileCredentialsProvider("aws.properties")).build();
+				break;
+			}
+			// create connection
+			QueueConnection connection = conFactory.createQueueConnection();
+			// create session
+			session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			Queue requestQueue = null;
+			Queue responseQueue = null;
+			switch (conf.mom) {
+			case ActiveMQ:
+				// lookup queue
+				requestQueue = (Queue) jndi.lookup(conf.requestQueueName);
+				responseQueue = (Queue) jndi.lookup(conf.responseQueueName);
+				break;
+			case SQS:
+			default:
+				requestQueue = session.createQueue(conf.requestQueueName);
+				responseQueue = session.createQueue(conf.responseQueueName);
+				break;
+			}
+			// create sender and receiver
+			sender = session.createSender(requestQueue);
+			receiver = session.createReceiver(responseQueue);
+			// start connection (!)
+			connection.start();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		env.put("connectionFactoryNames", "ConnectionFactory");
-		env.put("queue." + conf.requestQueueName, conf.requestQueueName);
-		env.put("queue." + conf.responseQueueName, conf.responseQueueName);
-		// create JNDI context (will also read jndi.properties file)
-		jndi = new InitialContext(env);
-		// connect to messaging system
-		conFactory = (QueueConnectionFactory) jndi.lookup("ConnectionFactory");
-		break;
-	    case SQS:
-	    default:
-		// Create the connection factory using the properties file
-		// credential provider.
-		conFactory = SQSConnectionFactory.builder().withRegion(Region.getRegion(Regions.EU_WEST_1))
-			.withAWSCredentialsProvider(new PropertiesFileCredentialsProvider("aws.properties")).build();
-		break;
-	    }
-	    // create connection
-	    QueueConnection connection = conFactory.createQueueConnection();
-	    // create session
-	    session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-	    Queue requestQueue = null;
-	    Queue responseQueue = null;
-	    switch (conf.mom) {
-	    case ActiveMQ:
-		// lookup queue
-		requestQueue = (Queue) jndi.lookup(conf.requestQueueName);
-		responseQueue = (Queue) jndi.lookup(conf.responseQueueName);
-		break;
-	    case SQS:
-	    default:
-		requestQueue = session.createQueue(conf.requestQueueName);
-		responseQueue = session.createQueue(conf.responseQueueName);
-		break;
-	    }
-	    // create sender and receiver
-	    sender = session.createSender(requestQueue);
-	    receiver = session.createReceiver(responseQueue);
-	    // start connection (!)
-	    connection.start();
-	} catch (NamingException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (JMSException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
 	}
-    }
 
-    @Override
-    public String process(String text) {
-	// TODO send message to request queue, receive response from response
+	@Override
+	public String process(String text) {
+		// TODO send message to request queue, receive response from response
 
-	// Create a text message like this:
-	// TextMessage msg = session.createTextMessage();
+		// Create a text message like this:
+		// TextMessage msg = session.createTextMessage();
 
-	// Add properties to a message like this:
-	// msg.setStringProperty("...", "...");
-	// Note: The text processor will copy all properties from the request
-	// message to the response message
+		// Add properties to a message like this:
+		// msg.setStringProperty("...", "...");
+		// Note: The text processor will copy all properties from the request
+		// message to the response message
 
-	// Receive messages either like this:
-	// receiver.receive();
-	// Or add a message listener instead:
-	// receiver.setMessageListener(new MyMessageLister());
-	
-	return text;
-    }
+		// Receive messages either like this:
+		// receiver.receive();
+		// Or add a message listener instead:
+		// receiver.setMessageListener(new MyMessageLister());
+
+		try {
+			TextMessage msg = session.createTextMessage();
+			msg.setText("test message");
+			msg.setStringProperty("type", "add");
+			msg.setStringProperty("value", "x");
+			sender.send(msg);
+			Message receivedMsg = receiver.receive(5000);
+			if (receivedMsg != null) {
+				System.out.println(((TextMessage) receivedMsg).getText());
+				@SuppressWarnings("unchecked")
+				Enumeration<String> propNames = msg.getPropertyNames(); 
+				while (propNames.hasMoreElements()) {
+					String pn = propNames.nextElement();
+					System.out.println(receivedMsg.getObjectProperty(pn));
+				}
+			} else {
+				System.err.println("TIMEOUT: No message received");
+			}
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+
+		
+		return text;
+	}
 
 }
